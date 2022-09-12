99858a2c740a0411f53b35e1a98657a0ea39e3cb
usktea <tjrxo1234@gmail.com>
2022-09-12 09:20:49 +0900

    20220912-음양더하기
    IntStream이용해서 풀었음.
    인스턴스 변수를 쓰지 않고 풀 수 있을까?

---
+++ 음양더하기/java/20220912/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.stream.IntStream;
+
+public class Solution {
+    int[] absolutes;
+    private boolean[] signs;
+    int index = 0;
+
+    public int solution(int[] absolutes, boolean[] signs) {
+        this.absolutes = absolutes;
+        this.signs = signs;
+
+        int answer = IntStream.of(absolutes)
+                .map(number -> changeSign(number))
+                .reduce(0, (a, b) -> a + b);
+
+        return answer;
+    }
+
+    private int changeSign(int number) {
+        if (signs[index] == false) {
+            return number * -1;
+        }
+
+        return number;
+    }
+}
