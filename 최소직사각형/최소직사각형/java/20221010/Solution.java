f0e4e9a4f89d38a7f05b5aa5230c1d72086f2fb8
usktea <tjrxo1234@gmail.com>
2022-10-10 09:26:57 +0900

    20221010-최소직사각형

---
+++ 최소직사각형/최소직사각형/java/20221010/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[][] sizes) {
+        int longest = longestLength(sizes);
+        int lastLongest = lastLongLength(sizes, longest);
+
+        return calculateArea(longest, lastLongest);
+    }
+
+    public int longestLength(int[][] sizes) {
+        int result = Arrays.stream(sizes)
+                .map(card -> Math.max(card[0], card[1]))
+                .reduce(0, (a, b) -> Math.max(a, b));
+
+        return result;
+    }
+
+    public int lastLongLength(int[][] sizes, int longestLength) {
+        int result = Arrays.stream(sizes)
+                .map(card -> Math.min(card[0], card[1]))
+                .reduce(0, (a, b) -> Math.max(a, b));
+
+        return result;
+    }
+
+    public int calculateArea(int longest, int lastLongest) {
+        return longest * lastLongest;
+    }
+}
