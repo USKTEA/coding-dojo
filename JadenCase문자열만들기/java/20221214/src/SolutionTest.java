8971f06b72898ecf20e1e1a6c1968813c07855c3
usktea <tjrxo1234@gmail.com>
2022-12-14 09:56:48 +0900

    포맷팅 수정

--- JadenCase문자열만들기/java/20221214/src/SolutionTest.java
+++ JadenCase문자열만들기/java/20221214/src/SolutionTest.java
@@ -1,19 +1,17 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
     @Test
-    void toChars() {
+    void toStrings() {
         Solution solution = new Solution();
         String[] strings = solution.toStrings("3people Unfollowed Me");
 
         assertArrayEquals(
-                new String[]{"3", "p", "e","o","p","l","e"," ","U","n","f","o","l","l","o","w","e","d"," ","M","e"},
+                new String[]{"3", "p", "e", "o", "p", "l", "e", " ", "U", "n", "f", "o", "l", "l", "o", "w", "e", "d", " ", "M", "e"},
                 strings);
     }
 
@@ -21,11 +19,11 @@ class SolutionTest {
     void toJadenCase() {
         Solution solution = new Solution();
 
-        String jadens1 = solution.toJaden(new String[]{"3", "p", "e","o", " ", "p","l","e"});
+        String jadens1 = solution.toJaden(new String[]{"3", "p", "e", "o", " ", "p", "l", "e"});
 
         assertEquals("3peo Ple", jadens1);
 
-        String jadens2 = solution.toJaden(new String[]{"3", "p", "e","o","p","l","e"," ","U","n","f","o","l","l","o","w","e","d"," ","M","e"});
+        String jadens2 = solution.toJaden(new String[]{"3", "p", "e", "o", "p", "l", "e", " ", "U", "n", "f", "o", "l", "l", "o", "w", "e", "d", " ", "M", "e"});
 
         assertEquals("3people Unfollowed Me", jadens2);
     }
