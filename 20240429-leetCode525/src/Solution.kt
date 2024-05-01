225392fbeb1d153c35e19d2e363e2d25dc23127b
usktea <tjrxo1234@gmail.com>
2024-05-01 11:57:09 +0900

    20240430-leetCode525

--- 20240429-leetCode525/src/Solution.kt
+++ 20240429-leetCode525/src/Solution.kt
@@ -1,2 +1,59 @@
 class Solution {
+    //[0, 1] => 2
+    //[0, 1, 0] => 1
+    //[0] => 0
+    //[1] => 0
+    //[0, 1, 0, 1] => 4
+    //[0, 1, 1, 0] => 4
+    //[0, 0, 1, 1] => 4
+    //mutableMap<Int, Record>()
+    //for(i: Int in nums.indices) {
+    //if(nums[i] == 0) {
+    //val record = mutalbeMap.getOrDefault(i, Record(0, 0))
+    //record.addOne, addZero
+    //mutableMap[i] = record
+    // }
+    //
+    //for (i: Int map.size - 1 down to 0) {
+    // map[i] = record
+    // if (record.isContiguous) {
+    // return record.size
+    // }
+    open fun findMaxLength(nums: IntArray): Int {
+        val map: MutableMap<Int, Int> = HashMap()
+        map[0] = -1
+        var maxlen = 0
+        var count = 0
+
+        //0 => -1
+        //-1 => 0
+        //count = 0
+        //maxLen = 2
+
+        //[0, 1, 0, 1]
+        //0
+        //1
+        //0
+        //1
+
+        for (i in nums.indices) {
+            if (nums[i] == 1) {
+                count += 1
+            }
+
+            if (nums[i] == 0) {
+                count -= 1
+            }
+
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.getValue(count))
+            }
+
+            if (!map.containsKey(count)) {
+                map[count] = i
+            }
+        }
+        return maxlen
+    }
 }
+
--- 20240429-leetCode525/src/SolutionTest.kt
+++ 20240429-leetCode525/src/SolutionTest.kt
@@ -1,3 +1,31 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class SolutionTest
+class SolutionTest {
+    private val solution = Solution()
+
+    @Test
+    fun when_nums_size_is_1() {
+        val result = solution.findMaxLength(intArrayOf(0))
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun when_nums_is_0_and_1() {
+        val result = solution.findMaxLength(intArrayOf(0, 1))
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun when_nums_is_0_and_1_and_0() {
+        val result = solution.findMaxLength(intArrayOf(0, 1, 0))
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun complex_test_case() {
+        val result = solution.findMaxLength(intArrayOf(0, 0, 1, 0, 0, 0, 1, 1))
+        assertEquals(6, result)
+    }
+}
+
